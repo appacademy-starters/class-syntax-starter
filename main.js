59b1aeb2bfe4311c0775b2a260a758df9b9ce7a6
Brady Fukumoto <bradyfukumoto@gmail.com>
2021-05-06 13:59:30 -0700

    Starter

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-// 2093 Short (15-20 minute) Pair programming exercise, Creating a class, Instance methods and variables, Class methods and variables
-
-// Update a class, Ingredient, with instance properties repesenting an 
-// ingredient name, cost, quantity, and unit of measurement.
-// Ingredient is located in `ingredient.js`
-
-// Give the class an instance method, printIngredient, that console.logs the
-// ingredient's details, including its name, cost, quantity, and unit.
-
-// Give the class a static method, printIngredientList, that takes an array of
-// Ingredient instances and calls printIngredient on every ingredient.
-
-// Give the class an instance method, calculateCost, that takes an integer
-// representing a quantity and calculates and returns the cost of the ingredient
-// for that quantity.
 
 const Ingredient = require("./ingredient.js");
 
